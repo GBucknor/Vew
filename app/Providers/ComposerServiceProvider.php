67c4ea375a15fc440edded314cdf88bdc2431925
Garel Bucknor <garelbucknor@gmail.com>
2018-04-09 22:34:30 -0700

    Added authentication for channel creation upon user registration. Started on implementing the channel view and channel settings views from the navigation bar.

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Sets the view that this composer will be interacting with.
+        view()->composer('layouts.partials._nav', \App\Http\ViewComposers\NavigationComposer::class);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
