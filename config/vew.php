7532119777270e30116d949900916bc4e42f7d70
Garel Bucknor <garelbucknor@gmail.com>
2018-04-20 14:14:40 -0700

    Added a video index so that users can see thier uploaded videos.

---
+++ config/vew.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: garel
+ * Date: 20/04/2018
+ * Time: 13:47
+ */
+
+return [
+    'buckets' => [
+        'videos' => 'https://s3.console.aws.amazon.com/s3/buckets/videos.vew.test',
+        'images' => 'https://s3.console.aws.amazon.com/s3/buckets/images.vew.test',
+    ],
+];
