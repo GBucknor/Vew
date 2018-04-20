1e04662f812c0327d16a687ff37706e8e2a9a35e
Garel Bucknor <garelbucknor@gmail.com>
2018-04-20 13:14:30 -0700

    Added an exception so that the webhook encoding from telestream cloud isn't rejected by the server.

--- app/Http/Controllers/EncodingWebhookController.php
+++ app/Http/Controllers/EncodingWebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class EncodingWebhookController extends Controller
@@ -16,15 +17,21 @@ class EncodingWebhookController extends Controller
 
     protected function videoEncoded(Request $request) {
         // Looks up the video in the database
-        $video = $this->getVideoByFileName();
+        $video = $this->getVideoByFileName($request->original_filename);
+        // Updates processed
+        $video->processed = true;
+        $video->video_id = $request->encoding_ids[0];
+
+        $video->save();
     }
 
     protected function encodingProgress(Request $request) {
-        //
+        $video = $this->getVideoByFileName($request->original_filename);
+        $video->processed_percent = $request->progress;
+        $video->save();
     }
 
-    private function getVideoByFileName()
-    {
-        //
+    private function getVideoByFileName($filename) {
+        return Video::where('video_filename', $filename)->firstOrFail();
     }
 }
